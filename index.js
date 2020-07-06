537b29a8e3fe33f20b6766ba5e37326647ca0ed6
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-07-06 15:16:35 -0500

    added cors

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const app = express();
 
 const jsonParser = bodyParser.json();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.post('/sms', jsonParser, (req, res) => {
 
 const accountSid = process.env.ACCOUNT_SID;
